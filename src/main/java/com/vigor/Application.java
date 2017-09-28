4f6bac39f9e7badbb8fa20ec7a5453cd008f4442
songlei <hsly_song@sina.com>
2017-09-28 23:12:20 +0800

    Spring Boot actuator

--- src/main/java/com/vigor/Application.java
+++ src/main/java/com/vigor/Application.java
@@ -2,8 +2,11 @@ package com.vigor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://www.xxx.com", methods = {RequestMethod.GET, RequestMethod.POST})
 public class Application {
 
 	public static void main(String[] args) {
