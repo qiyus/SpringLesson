5f42abd834423b68602f6383f34d3aa5ead9e5ab
songlei <hsly_song@sina.com>
2017-12-10 18:42:18 +0800

    调整测试用例，每次只测试一个问题。

--- src/test/java/com/vigor/ApplicationTests.java
+++ src/test/java/com/vigor/ApplicationTests.java
@@ -1,15 +1,20 @@
 package com.vigor;
 
+import com.vigor.redis.RedisTest;
+import com.vigor.statemachine.StateMachineTest;
+import com.vigor.task.AsyncTaskTest;
 import com.vigor.web.controller.WebControllerTest;
 import com.vigor.web.controller.UserControllerTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.springframework.boot.actuate.metrics.export.MetricExportProperties;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        PropertiesControllerTest.class,
         UserControllerTest.class,
-        WebControllerTest.class
+        WebControllerTest.class,
+        RedisTest.class,
+        StateMachineTest.class
 })
 public class ApplicationTests {
 }
--- src/test/java/com/vigor/statemachine/StateMachineTest.java
+++ src/test/java/com/vigor/statemachine/StateMachineTest.java
@@ -9,6 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
+import static com.vigor.statemachine.OrderState.DONE;
+import static com.vigor.statemachine.OrderState.UNPAID;
+import static com.vigor.statemachine.OrderState.WAITING_FOR_RECEIVE;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by Vigor on 2017/11/4.
  */
@@ -20,9 +26,20 @@ public class StateMachineTest {
     private StateMachine stateMachine;
 
     @Test
-    public void orderProcess() throws Exception{
+    public void start() throws Exception{
         stateMachine.start();
+        assertThat(stateMachine.getState().getId(), is(UNPAID));
+    }
+
+    @Test
+    public void pay() throws Exception {
         stateMachine.sendEvent(OrderEvent.PAY);
+        assertThat(stateMachine.getState().getId(), is(WAITING_FOR_RECEIVE));
+    }
+
+    @Test
+    public void receive() throws Exception {
         stateMachine.sendEvent(OrderEvent.RECEIVE);
+        assertThat(stateMachine.getState().getId(), is(DONE));
     }
 }
